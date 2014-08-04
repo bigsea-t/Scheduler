3d3047464544f14553e2e8b3551c8435f9cd81ec
taikai <297.1951@gmail.com>
2014-08-05 01:21:40 +0900

    Avoid upcase in email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 30 }
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
