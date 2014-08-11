5308badfaca1fb74d34f5714239fa238cd7e3ae7
taikai <297.1951@gmail.com>
2014-08-11 18:04:55 +0900

    Add attender and owner to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
-    has_many :users
+    has_many :event_users, foreign_key:"attender_id", dependent: :destroy
+    has_many :attend_users, through: :event_users, source: :attender
     validates :name, presence: true
     validates :date, presence: true
     default_scope -> { order('date DESC') }
--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -1,2 +1,8 @@
 class EventUser < ActiveRecord::Base
+  belongs_to :owner,    class_name: "Event"
+  belongs_to :attender, class_name: "Event"
+  validates  :owner_id,    presence: true
+  validates  :attender_id, presemce: true
+  validates  :event_id,    presence: true
+  #TODO: add attend_events
 end
